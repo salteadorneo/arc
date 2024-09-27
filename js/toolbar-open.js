c50b529c8a18a5548d56d8c6db91c247995caec1
Cristian Adán <hola@salteadorneo.dev>
2024-09-27 21:56:55 +0200

    Refactor toolbar transition duration in toolbar.css

--- js/toolbar-open.js
+++ js/toolbar-open.js
@@ -1,6 +1,6 @@
 if (document.querySelector('toolbar')) {
-  if (!document.querySelector('toolbar').style.maxHeight || document.querySelector('toolbar').style.maxHeight === '0px') {
-    document.querySelector('toolbar').style.maxHeight = '50px'
+  if (document.querySelector('toolbar').clientHeight === 0) {
+    document.querySelector('toolbar').style.maxHeight = '500px'
   } else {
     document.querySelector('toolbar').style.maxHeight = '0px'
   }
